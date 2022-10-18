badad1bd013dc0a7f34c292090d0e0380d1f6b74
Ryan Baxter <524254+ryanjbaxter@users.noreply.github.com>
2022-10-18 11:07:27 -0400

    Use simplediscoveryclient

--- order-service/src/main/java/com/example/orderservice/order/OrderResource.java
+++ order-service/src/main/java/com/example/orderservice/order/OrderResource.java
@@ -1,12 +1,17 @@
 package com.example.orderservice.order;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import java.net.URI;
+import java.util.List;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
-import java.net.URI;
-import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(OrderResource.BASE_URI)

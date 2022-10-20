cdc9cf5a213be6c35e0fb3b51c3218e2245e2c2d
Ryan Baxter <524254+ryanjbaxter@users.noreply.github.com>
2022-10-20 16:30:50 -0400

    Defining bean

--- gateway/src/main/java/com/example/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/example/gateway/GatewayApplication.java
@@ -2,11 +2,23 @@ package com.example.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.kubernetes.discovery.KubernetesDiscoveryClientProperties;
+import org.springframework.cloud.kubernetes.discovery.KubernetesReactiveDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class GatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
+    }
+
+    @Bean
+    public ReactiveDiscoveryClient kubernetesReactiveDiscoveryClient(WebClient.Builder webClientBuilder,
+            KubernetesDiscoveryClientProperties properties) {
+        return new KubernetesReactiveDiscoveryClient(webClientBuilder, properties);
     }
 }
